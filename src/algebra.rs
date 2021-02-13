a2562bf24e79b1945ac8acbb06ec0818973510b2
sander.bockstaele <sander.bockstaele@gmail.com>
2021-02-13 21:10:23 +0100

    Remove MyError.
    I don't use it.

--- src/algebra.rs
+++ src/algebra.rs
@@ -1,42 +1,13 @@
-use std::error::Error;
-use std::fmt;
-
 extern crate pest;
+extern crate emath;
 
 use pest::Parser;
+use emath::Pos2;
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
 struct IdentParser;
 
-#[derive(Debug)]
-pub struct MyError {
-    details: String
-}
-
-impl MyError {
-    fn new(msg: &str) -> MyError {
-        MyError{details: msg.to_string()}
-    }
-}
-
-impl fmt::Display for MyError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f,"{}",self.details)
-    }
-}
-
-impl Error for MyError {
-    fn description(&self) -> &str {
-        &self.details
-    }
-}
-
-pub fn solve_equation(equation: String) -> Result<i64, MyError> {
-	if equation.is_empty() {
-		return Err(MyError::new("the given equation is empty"))
-	} else {
-		return Ok(0)
-	}
+pub fn solve_equation(equation: String) -> Vec<Pos2> {
 
 }
