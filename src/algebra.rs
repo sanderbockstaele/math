b45f559916877b9ab1fe449b31c192522160af90
sander.bockstaele <sander.bockstaele@gmail.com>
2021-02-13 22:19:13 +0100

    Remove pest
    It isn't used.

--- src/algebra.rs
+++ src/algebra.rs
@@ -1,13 +1,11 @@
-extern crate pest;
 extern crate emath;
 
-use pest::Parser;
 use emath::Pos2;
 
-#[derive(Parser)]
-#[grammar = "grammar.pest"]
-struct IdentParser;
-
 pub fn solve_equation(equation: String) -> Vec<Pos2> {
+    let graph = vec![
 
+    ];
+
+    return graph;
 }
