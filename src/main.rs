f0f76554b1439c75fc78cdf33fae4673d6c831ec
sander bockstaele <sander.bockstaele@gmail.com>
2021-03-14 14:17:30 +0100

    Skip the window part and make it so that it direclty calls solve_equation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-mod algebra;
 mod app;
+mod algebra;
 
 extern crate eframe;
 
 use crate::app::App;
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
     let app = App::default();
-    eframe::run_native(Box::new(app));
+    // eframe::run_native(Box::new(app));
+
+    algebra::solve_equation(&args[1]);
 }
