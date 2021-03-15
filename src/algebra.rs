235507092d7f85bc89fc5115d4bcd58a6cdf3535
sander bockstaele <sander.bockstaele@gmail.com>
2021-03-15 17:53:15 +0100

    Improve the is_number function to not spew errors.
    it has some problems, see comment in function.

--- src/algebra.rs
+++ src/algebra.rs
@@ -1,5 +1,16 @@
 use unicode_segmentation::UnicodeSegmentation;
 use emath::Pos2;
+use array2d::Array2D;
+
+const LETTER: Vec<char> = vec!['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'
+,'q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M',
+'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+struct Token<'a> {
+    name: &'a str,
+    token: Tokens,
+    value: &'a str,
+}
 
 enum Tokens {
     Function,
@@ -10,12 +21,27 @@ enum Tokens {
 // used if either it is a function or a variable
 fn is_function(characters: Vec<&str>) -> bool {
     let mut iterator = characters.iter().peekable();
-    
+
     while iterator.peek() != None {
         if iterator.peek().unwrap() == &&"(" {
             return true;
         }
         iterator.next();
+    }
+
+    return false;
+}
+
+fn is_number(characters: Vec<&str>) -> bool {
+    let mut iterator = characters.iter().peekable();
+
+    while iterator.peek() != None { 
+        for character in LETTER {
+            let current_character: &str = &character.to_string();
+            if iterator.peek().unwrap() == &&current_character{
+                       
+            }
+        }
     }
 
     return false;
