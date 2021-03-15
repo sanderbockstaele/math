bb79e78052ec6e58d20874c9142e180a10b676dd
sander bockstaele <sander.bockstaele@gmail.com>
2021-03-15 10:39:08 +0100

    create a system to discribe a the equation

--- src/algebra.rs
+++ src/algebra.rs
@@ -1,10 +1,31 @@
 use unicode_segmentation::UnicodeSegmentation;
-use prev_iter::PrevPeekable;
 use emath::Pos2;
+
+enum Tokens {
+    Function,
+    Variable,
+    Number,
+}
+
+// used if either it is a function or a variable
+fn is_function(characters: Vec<&str>) -> bool {
+    let mut iterator = characters.iter().peekable();
+    
+    while iterator.peek() != None {
+        if iterator.peek().unwrap() == &&"(" {
+            return true;
+        }
+        iterator.next();
+    }
+
+    return false;
+}
 
 pub fn solve_equation(equation: &str) -> Vec<Pos2> {
     // 
     let characters = UnicodeSegmentation::graphemes(equation, true).collect::<Vec<&str>>();
+
+    is_function(characters);
 
     let graph = vec![
 
