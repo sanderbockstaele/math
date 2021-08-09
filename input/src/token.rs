943a7226cc2d8fba4e95244c97088ef4317e9af5
Sander Bockstaele <sander.bockstaele@gmail.com>
2021-08-09 16:53:10 +0200

    Remove unused LETTERS variable

--- input/src/token.rs
+++ input/src/token.rs
@@ -1,10 +1,6 @@
 use unicode_segmentation::UnicodeSegmentation;
 use std::fmt::Display;
 
-const LETTER: [char; 52] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'
-,'q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M',
-'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-
 const OPERATION: [char; 4] = ['+','-','*','/'];
 
 const NUMBER: [char; 10] = ['0','1','2','3','4','5','6','7','8','9'];
